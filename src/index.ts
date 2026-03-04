62f5a5d7f7e9105d33c47bf4b93c489b512b19d7
Fabio <fabiocaldas266@gmail.com>
2026-03-04 20:12:06 -0300

    chore: add eslint, fastify and  swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-console.log("Hello, World!")
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+
+const app = Fastify({
+  logger: true,
+});
+
+const PORT = Number(process.env.PORT) || 8080;
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para gerenciamento de treinos do Bootcamp",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "LocalHost",
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
+try {
+  await app.listen({ port: PORT });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
