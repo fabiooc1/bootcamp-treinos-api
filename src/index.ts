73a85c22c1d8312c49af0c700cded9842d4278ba
Fabio <fabiocaldas266@gmail.com>
2026-03-04 21:43:16 -0300

    chore: better auth setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
@@ -6,6 +7,8 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+
+import { auth } from "./lib/auth.js";
 
 const app = Fastify({
   logger: true,
@@ -35,6 +38,44 @@ await app.register(fastifySwagger, {
 
 await app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
+});
+
+await app.register(fastifyCors, {
+  origin: ["http://localhost:5173", "http://localhost:3000"],
+  credentials: true,
+});
+
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      const url = new URL(request.url, `http://${request.headers.host}`);
+
+      const headers = new Headers();
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+      const response = await auth.handler(req);
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(
+        error instanceof Error ? error : new Error("Unknown error"),
+      );
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
+    }
+  },
 });
 
 try {
